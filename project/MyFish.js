77641ac51c33b0167fab2db82994e8cce4aa3343
Afonso Monteiro <afonso.dc.monteiro@outlook.pt>
2021-04-29 16:35:06 +0100

    Added objects needed for fish

--- project/MyFish.js
+++ project/MyFish.js
@@ -1,12 +1,18 @@
 import {CGFobject} from '../lib/CGF.js';
 import {MyMovingObject} from './MyMovingObject.js';
 import {MySphere} from './MySphere.js';
+import {MyTriangleBig} from './MyTriangleBig.js';
+import {MyTriangleSmall} from './MyTriangleSmall.js';
 
 export class MyFish extends MyMovingObject
 {
     constructor(scene, slices, stacks)
     {
         super(scene, slices, stacks);
+        this.body = new MySphere(scene, slices, stacks);
+        this.rightFin = new MyTriangleSmall(scene);
+        this.leftFin = new MyTriangleSmall(scene);
+        this.swimmerFin = new MyTriangleBig(scene);
         this.initBuffers();
     }
 }
