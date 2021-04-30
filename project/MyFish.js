acafb53ba39073c2b25e86af771f7fed46b8a00d
Afonso Monteiro <afonso.dc.monteiro@outlook.pt>
2021-04-30 11:16:21 +0100

    body declared - fish development part 2

--- project/MyFish.js
+++ project/MyFish.js
@@ -10,9 +10,23 @@ export class MyFish extends MyMovingObject
     {
         super(scene, slices, stacks);
         this.body = new MySphere(scene, slices, stacks);
+        this.eye1 = new MySphere(scene, slices, stacks);
+        this.eye2 = new MySphere(scene, slices, stacks);
         this.rightFin = new MyTriangleSmall(scene);
         this.leftFin = new MyTriangleSmall(scene);
+        this.topFin = new MyTriangleSmall(scene);
         this.swimmerFin = new MyTriangleBig(scene);
-        this.initBuffers();
+    }
+
+    display()
+    {
+        //body (how to force required dimensions?)
+        this.scene.pushMatrix();
+        this.scene.scale(2,1,1);
+        this.scene.translate(0,3,0);
+        this.body.display();
+        this.scene.popMatrix();
+
+
     }
 }
